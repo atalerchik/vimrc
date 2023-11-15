870d6108fef51eded7a43ee5f213ebc15f5def9f
Alexey Talerchik <atalerchik@twelvedevs.com>
2023-11-15 09:19:42 +0300

    Initialize repository

---
+++ lua/keymappings.lua
@@ -0,0 +1,63 @@
+-- Make Space <leader> key
+vim.g.mapleader = " "
+
+local keymap = vim.keymap;
+
+-- quickfix items
+keymap.set('n', '<leader>h', '<cmd>cnext<CR>zz')
+keymap.set('n', '<leader>;', '<cmd>cprev<CR>zz')
+
+keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
+keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+
+-- Open NvimTree explorer
+keymap.set("n", "<leader>pv", '<cmd>NvimTreeOpen<CR>')
+
+-- move lines
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- center screen 
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+-- safe past from buffer
+keymap.set("x", "<leader>p", [["_dP]])
+
+-- copy in system registry
+keymap.set({"n", "v"}, "<leader>y", [["+y]])
+keymap.set("n", "<leader>Y", [["+Y]])
+
+-- safe delete
+keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+keymap.set("n", "<leader>,", ":noh<CR>")
+
+
+keymap.set('n', '<space>e', vim.diagnostic.open_float)
+keymap.set('n', '[d', vim.diagnostic.goto_prev)
+keymap.set('n', ']d', vim.diagnostic.goto_next)
+keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Map 'jk' to <esc> in insert mode
+vim.api.nvim_set_keymap('i', 'jk', '<esc>', {})
+
+-- Obsidian 
+keymap.set('n', '<leader>ot', ':ObsidianToday<CR>')
+keymap.set('n', '<leader>oy', ':ObsidianYesterday<CR>')
+keymap.set('n', '<leader>obl', ':ObsidianBacklinks<CR>')
+
+-- Maker dir for this file
+keymap.set('n', '<leader>md', ':!mkdir -p %:h<CR>', { noremap = true, silent = true })
+
+-- Add javascript console loge for current word or section
+keymap.set('n', '<leader>cl', 'yiwoconsole.log("", <C-r>o);<ESC>', { noremap = true, silent = true })
+keymap.set('v', '<leader>cl', 'yOconsole.log("", <C-r>"");<ESC>', { noremap = true, silent = true })
+
+keymap.set('v', '<leader>wf', 'Sfunction () {<CR><C-r>"<CR>}<ESC>', { noremap = true, silent = true })
+
+keymap.set('n', '<leader>zz', ':ZenMode<cr>', { noremap = true, silent = true })
+
+keymap.set('n', '<leader>ex', ':NvimTreeOpen<cr>', { noremap = true, silent = true })
---
+++ lua/settings.lua
@@ -0,0 +1,63 @@
+local opt = vim.opt
+
+-- Enable mouse
+vim.o.mouse = 'a'
+
+-- Set encoding
+vim.o.encoding = 'utf-8'
+
+-- Enable line numbers
+vim.opt.relativenumber = true
+vim.wo.number = true
+
+-- Disable swap file
+vim.o.swapfile = false
+
+-- Set scrolloff
+vim.o.scrolloff = 7
+
+-- Set tabstop, softtabstop, shiftwidth, and expandtab
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+-- Enable autoindent
+vim.o.autoindent = true
+vim.o.smartindent = true
+
+-- Set fileformat
+vim.o.fileformat = 'unix'
+
+-- Set numberwidth
+vim.o.numberwidth = 2
+
+-- Enable filetype-specific indent files
+vim.cmd('filetype indent on')
+
+-- Netrw settings
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 3
+
+-- Line wrap 
+opt.wrap = false
+
+-- Search settings
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+opt.incsearch = false
+
+-- Apperance
+opt.termguicolors = true
+
+-- Vim markdown (vim-markdown-set-header-folding-level)
+vim.g.vim_markdown_folding_level = 6
+
+
+opt.expandtab = false;
+
+-- disable netrw at the very start of init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
